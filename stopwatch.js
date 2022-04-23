6ad56184098837cc7b606fb206f9c77985c11894
Ervin <nikolliervin@gmail.com>
2022-04-23 23:11:54 +0200

    final backend

--- stopwatch.js
+++ stopwatch.js
@@ -22,7 +22,7 @@ stopBtn.addEventListener('click', ()=>{
 resetBtn.addEventListener('click',()=>{
     clearInterval(int);
     [milliseconds,seconds,minutes,hours] = [0,0,0,0];
-    timerRef.innerHTML = '00 : 00 : 00 : 000 ';
+    timerRef.innerHTML = '00 : 00 : 00';
 })
 
 function displayTimer(){
@@ -47,6 +47,6 @@ let m = minutes < 10 ? "0" + minutes : minutes;
 let s = seconds < 10 ? "0" + seconds : seconds;
 let ms = milliseconds < 10 ? "00" + milliseconds : milliseconds < 100 ? "0" + milliseconds : milliseconds;
 
-timerRef.innerHTML = ` ${h} : ${m} : ${s} : ${ms}`;
+timerRef.innerHTML = ` ${h} : ${m} : ${s} `;
 }
 
